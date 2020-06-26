6a451855a1711775b1098db8bc2f9f87b769cbc6
jhonperez95 <jhonperez.j95@gmail.com>
2020-06-25 21:37:26 -0500

    add enzyme

--- src/tests/counterApp.test.js
+++ src/tests/counterApp.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import CounterApp from '../components/CounterApp/CounterApp';
+import { shallow } from 'enzyme';
 
 describe('Pruebas al componente <CounterApp>', () => {
-  test('Debe enviar un numero por parametro Props', () => {
-    const num = 4;
+  // test('Debe enviar un numero por parametro Props', () => {
+  //   const num = 4;
+  //   const { getByText } = render(<CounterApp value={num} />);
+  //   // expect(getByText(num)).toBeInTheDocument();
+  // });
 
-    const { getByText } = render(<CounterApp value={num} />);
-
-    // expect(getByText(num)).toBeInTheDocument();
+  test('Renderizado correctamente de <CounterApp/>', () => {
+    const count = 5;
+    const wrapper = shallow(<CounterApp value={count} />);
+    expect(wrapper).toMatchSnapshot();
   });
 });
