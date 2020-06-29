c8e08c5e1846d6d8874e494456c77d70a22193a5
Jhon Perez <jhonperez.j95@gmail.com>
2020-06-28 19:39:01 -0500

    update

--- src/base-pruebas/11-async-await.js
+++ src/base-pruebas/11-async-await.js
@@ -8,9 +8,7 @@ export const getImagen = async () => {
       `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
     );
     const { data } = await resp.json();
-
     const { url } = data.images.original;
-
     return url;
   } catch (error) {
     // manejo del error
